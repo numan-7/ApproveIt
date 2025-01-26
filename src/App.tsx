25b98484aaa417312f46d9974eb7759caad5602e
Numan Khan <101899800+numan-7@users.noreply.github.com>
2025-01-26 13:17:48 -0800

    feat: font, imgs, tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <p className='text-2xl'>test</p>
+      <p className='text-2xl font-main'>ApproveIt</p>
     </>
   )
 }
