7fe548febc9ba1eb79ab68831e0bbcfa096b2c5a
numan-7 <101899800+numan-7@users.noreply.github.com>
2025-02-11 09:47:31 -0800

    feat: add dashboard numbers

---
+++ app/api/approvals/route.ts
@@ -0,0 +1,54 @@
+import { NextResponse } from 'next/server';
+import { createClientForServer } from '@/utils/supabase/server';
+
+export async function GET(req: Request) {
+    const supabase = await createClientForServer();
+
+    const {
+      data: { user },
+      error: userError,
+    } = await supabase.auth.getUser();
+    
+    if (userError || !user)
+      return NextResponse.json({ error: 'Not authenticated' }, { status: 403 });
+  
+    const { data: incoming, error: incomingError } = await supabase.rpc('get_approvals_for_user', {
+        user_email: user.email,
+    });
+
+    const { data: outgoing, error: outgoingError } = await supabase
+    .from('approvals')
+    .select(
+      `
+      id,
+      name,
+      requester,
+      date,
+      description,
+      status,
+      priority,
+      approvers,
+      comments: comments ( id, name, user_email, comment, created_at ),
+      attachments: attachments ( name, type, size, url )
+    `
+    )
+    .eq('requester', user.email)
+    .order('date', { ascending: true });
+
+    const incomingApprovalslength = incoming.length || 0;
+    const outgoingApprovalslength = outgoing?.length || 0;
+
+
+    if ( incomingError ) {
+        return NextResponse.json({ error: incomingError.message }, { status: 500 });
+    } else if ( outgoingError ) {
+        return NextResponse.json({ error: outgoingError.message }, { status: 500 });
+    }
+
+    const data = {
+        incoming: incomingApprovalslength,
+        outgoing: outgoingApprovalslength
+    }
+
+    return NextResponse.json(data);
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,51 @@
+"use client";
+
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { PlusCircle, ClipboardList, CheckSquare } from 'lucide-react';
 import { SummaryCards } from '@/components/dashboard/summary-cards';
+import { useEffect, useState } from 'react';
+import { SpinnerLoader } from '@/components/ui/spinner-loader';
 
 export default function Dashboard() {
+
+  interface ApprovalNumbers {
+    incoming: number;
+    outgoing: number;
+  }
+
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [approvalStats, setApprovalStats] = useState<ApprovalNumbers>({
+    incoming: 0,
+    outgoing: 0
+  });
+
+  useEffect(() => {
+    const fetchStats = async () => {
+      try {
+        const response = await fetch('/api/approvals');
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+        setApprovalStats(json)
+      } catch (error) {
+        console.error('Error fetching approvals:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchStats();
+  }, []);
+  
+  if (isLoading) return <SpinnerLoader />
+
   return (
     <div className="p-4 space-y-4">
       <h1 className="text-3xl font-bold">Dashboard</h1>
 
-      <SummaryCards myRequestsCount={5} pendingApprovalsCount={3} />
+      <SummaryCards myRequestsCount={approvalStats.outgoing} pendingApprovalsCount={approvalStats.incoming} />
 
       <div className="grid md:grid-cols-3 gap-6">
         <Card>
