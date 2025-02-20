1b9c08204aee62cf7afb76af9a239002799b1d7c
Numan Khan <101899800+numan-7@users.noreply.github.com>
2025-02-20 10:23:48 -0800

    pls now?

--- app/dashboard/approvals/create/page.tsx
+++ app/dashboard/approvals/create/page.tsx
@@ -1,13 +1,16 @@
-import { ApprovalForm } from "@/components/dashboard/approval-form";
+import { ApprovalForm } from '@/components/dashboard/approval-form';
+import { Suspense } from 'react';
+import { SpinnerLoader } from '@/components/ui/spinner-loader';
 
 export default function CreateApproval() {
   return (
-    <div className="min-h-full bg-white">
-        <div className ="p-4 space-y-4">
-            <h1 className="text-3xl font-bold">Create New Approval</h1>
-            <ApprovalForm />
+    <Suspense fallback={<SpinnerLoader />}>
+      <div className="min-h-full bg-white">
+        <div className="p-4 space-y-4">
+          <h1 className="text-3xl font-bold">Create New Approval</h1>
+          <ApprovalForm />
+        </div>
       </div>
-    </div>
-  )
+    </Suspense>
+  );
 }
-
