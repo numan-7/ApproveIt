2af69551900d6f93592095a221898f8483b232c4
numan-7 <101899800+numan-7@users.noreply.github.com>
2025-04-03 19:17:23 -0700

    feat: feedback form

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,19 @@
+import Link from 'next/link';
+
 export default function Footer() {
   return (
-    <footer className=" mb-2 absolute bottom-0 left-0 right-0 z-50">
-      <p className="text-sm text-white/60 text-center">
-        &copy; 2024 ApproveIt. All rights reserved.
+    <footer className="w-full absolute bottom-0 left-0 right-0 z-50 py-2">
+      <p className="text-center text-sm text-white/70 px-4">
+        &copy; 2025 ApproveIt &nbsp;|&nbsp; Feedback?{' '}
+        <Link
+          className="hover:underline"
+          href="https://docs.google.com/forms/d/e/1FAIpQLSewnrwEvl8wamQs_PZ6s1LRZ3PHbX66pTj8Pf6toMJj4RSKOQ/viewform?usp=header"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click here
+        </Link>
+        .
       </p>
     </footer>
   );
